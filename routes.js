54f8bf0f0e36350b9a80507f5123de86ffc63193
Goulin Khoge <goulin.khoge@gmail.com>
2019-10-08 11:17:22 +0200

    Routes implementation is done

--- routes.js
+++ routes.js
@@ -1,2 +1,47 @@
 const router = (exports.router = require("express").Router());
-router.get("/", (_, res) => res.send("yo"));
+
+const {
+  updateRooms,
+  searchRooms,
+  getRoomsByBuilding
+} = require("./services/room");
+const { updateAllplannings, getFreePlannings } = require("./services/planning");
+
+const buildings = require("./static/buildings");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const { place, date } = req.query;
+    if (!place && !date) {
+      throw new Error(
+        "[custom]Il faut donner une salle ou un bâtiment (query: place=exp&[date=YYYY-MM-DD])"
+      );
+    }
+    let rooms = await searchRooms(place);
+    // eslint-disable-next-line require-atomic-updates
+    rooms = (await getFreePlannings(rooms, date)).map((freeSchedules, i) => ({
+      room: rooms[i].name,
+      building: rooms[i].building,
+      freeSchedules
+    }));
+    res.json(rooms);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/buildings", (_req, res, next) => {
+  try {
+    res.json(buildings);
+  } catch (e) {
+    next(e);
+  }
+});
+router.get("/rooms/:building", async (req, res, next) => {
+  try {
+    const { building } = req.params;
+    res.json(await getRoomsByBuilding(building));
+  } catch (e) {
+    next(e);
+  }
+});
